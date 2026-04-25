e9563d69a4591de4fb9817ee9e0a597a07327596
mizz <mroytinkercad@gmail.com>
2026-04-24 20:38:10 -0500

    Delete register.js

--- register.js
+++
@@ -1,5 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js", { scope: "/" }).catch(err => {
-    console.error("Eclipse CDN service worker failed to register:", err);
-  });
-}
